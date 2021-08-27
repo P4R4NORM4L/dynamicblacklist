34384e88195ffd515312543063e3e3f3d269d87c
Hagen Hyena <hagen@paw.bot>
2021-08-27 04:18:01 +0100

    include repo details for reference

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,11 @@
 fx_version 'cerulean'
 game 'gta5'
 
+version '0.1.1'
+author 'Reece Stokes <hagen@hyena.gay>'
+description 'FiveM resource for ACE-specific blacklisting.'
+repository 'https://github.com/MrGriefs/dynamicblacklist'
+
 client_scripts {
     'weapontypes.lua',
     'config.lua',
